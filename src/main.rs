9519fbd0662f49924d38252f52b36dbcd3e28b2d
Raj Patel <raj17ce@gmail.com>
2024-11-19 16:54:43 +0530

    Use iterators

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,13 @@ use minigrep::{run, Config};
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
