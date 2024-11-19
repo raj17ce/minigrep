d7b5530498a8b216d28a0958093e66816cf915cf
Raj Patel <raj17ce@gmail.com>
2024-11-19 12:18:22 +0530

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::{env, fs};
+use std::{env, process};
+use minigrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for \"{query}\" In file {file_path}");
+    println!("Searching for \"{}\" In file {}", config.query, config.file_path);
 
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
